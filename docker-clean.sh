b4f1f357f90e1c87dca7aaf3547992f6c3a38b95
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-03-09 13:32:53 +0900

    Add xdebug from vscode

--- docker-clean.sh
+++ docker-clean.sh
@@ -5,11 +5,13 @@
 #######################
 echo **** clean docker ****
 docker-compose stop
-docker-compose rm
+docker-compose rm -f
 
 echo **** remove files ****
+rm -rf ./php/log/*
 rm -rf ./mysql/lib/*
 rm -rf ./phpmyadmin/sessions/*
 
 echo **** start ****
+docker build -t php_study_php ./php
 docker-compose up -d
